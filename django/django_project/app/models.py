f59549009c7cc479ea0195e4b76c03e778f4b6a4
Rob Majuri <robmajuri@macbook-air.myfiosgateway.com>
2023-04-18 14:22:24 -0400

    Add new models

--- django/django_project/app/models.py
+++ django/django_project/app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Creator(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    email = models.CharField(max_length=200, blank=True, null=True)
+    city = models.CharField(max_length=200, blank=True, null=True)
+    state = models.CharField(max_length=200, blank=True, null=True)
+    country = models.CharField(max_length=200, blank=True, null=True)
+    website = models.CharField(max_length=200, blank=True, null=True)
+    twitter = models.CharField(max_length=200, blank=True, null=True)
+    facebook = models.CharField(max_length=200, blank=True, null=True)
+    instagram = models.CharField(max_length=200, blank=True, null=True)
+    linkedin = models.CharField(max_length=200, blank=True, null=True)
+    youtube = models.CharField(max_length=200, blank=True, null=True)
+    tiktok = models.CharField(max_length=200, blank=True, null=True)
+    snapchat = models.CharField(max_length=200, blank=True, null=True)
+    twitch = models.CharField(max_length=200, blank=True, null=True)
+
+class Post(models.Model):
+    creator = models.ForeignKey('app.Creator', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, blank=True, null=True)
+    description = models.CharField(max_length=200, blank=True, null=True)
+    image = models.CharField(max_length=200, blank=True, null=True)
+    video = models.CharField(max_length=200, blank=True, null=True)
+    audio = models.CharField(max_length=200, blank=True, null=True)
+    link = models.CharField(max_length=200, blank=True, null=True)
+    date = models.DateTimeField(auto_now_add=True)
+    likes = models.IntegerField(blank=True, null=True)
+    comments = models.IntegerField(blank=True, null=True)
+    shares = models.IntegerField(blank=True, null=True)
--- django/django_project/settings.py
+++ django/django_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_project.app'
 ]
 
 MIDDLEWARE = [
@@ -129,7 +130,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
 }
